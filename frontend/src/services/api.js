2e2b7dced51888ac6ff95e936b857f2151a40dd7
Tu Nombre <167768452+AgusVuku7@users.noreply.github.com>
2026-03-20 14:02:46 -0300

    conectando front a la URL de Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL base de tu API
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://stock-app-backend-cg7w.onrender.com/';
 
 // Crear instancia de axios con configuración
 const api = axios.create({
